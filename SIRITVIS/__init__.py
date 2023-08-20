1bdcebfcd72a82be93d051f4ac49cde3a96e5372
Sagar Narwade <139004320+CodeEagle22@users.noreply.github.com>
2023-08-20 20:50:38 +0200

    Update __init__.py

--- SIRITVIS/__init__.py
+++ SIRITVIS/__init__.py
@@ -1,6 +1,6 @@
 from .reddit_streamer import RedditStreamer
 from .twitter_streamer import TwitterStreamer
 from .cleaner import Cleaner
-from .topic_visualise import pyLDAvis, TopicWizardvis
+from .topic_visualise import pyLDAvis, WordCloud
 from .topic_model import TopicModeling
 from .tweet_mapper import TweetMapper
